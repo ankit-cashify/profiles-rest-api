900afd04ec1ad447ea99c0fbfb2214ff7a936f9d
Ankit Kumar <ankit.kumar@cashify.in>
2021-10-06 18:03:57 +0530

    Added Custome user model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class UserProfilesManager(BaseUserManager):
+    """ Manager for user creation """
+
+    def create_user(self, email, name, password=None):
+        """ Create new user profiles """
+        if not email:
+            raise ValueError("user must have email id")
+            
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using =self._db)
+
+        return user
+
+    def create_super_user(self, email, name, password):
+        """ Create and save super with given details """
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff= True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfiles(AbstractBaseUser, PermissionsMixin):
+    """Database Models for user in System """
+    email = models.EmailField(max_length = 255, unique = True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    obejcts = UserProfilesManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user """
+        return self.name
+
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Retrive string represtation of our user """
+        return self.email
--- profiles_project/asgi.py
+++ profiles_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for profiles_rest project.
+ASGI config for profiles_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profiles_rest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profiles_project.settings')
 
 application = get_asgi_application()
